46321171ebb0a100314f4e0004cb7fbd3b5ad6b0
Anthony <anthony.wang@me.com>
2019-08-24 21:56:47 -0700

    New version deployed

--- javascript/navbar.js
+++
@@ -1,8 +0,0 @@
-function toggleNavbar() {
-	var myTopnav = document.getElementById("myTopnav");
-	if (myTopnav.classList.contains("responsive")) {
-		myTopnav.classList.remove("responsive");
-	} else {
-		myTopnav.classList.add("responsive");
-	}
-}
